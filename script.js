68c26dedc9be87e8dc29655c03619e03f867ccf9
tabby8612 <tabishsajwani@hotmail.com>
2024-03-25 14:11:59 +0400

    changed script.js and style.css

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const icon = document.getElementById("icon");
 
 
+if (localStorage.getItem("theme") === null) {
+    localStorage.setItem("theme", "light");
+} else if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.toggle("dark-theme");
+    document.querySelector("nav i").classList.replace("fa-moon", "fa-sun");
+    document.querySelector("nav i").style.color = "white";
+    localStorage.setItem("theme", "dark");
+}
+
+
 icon.addEventListener("click", () => {
     document.body.classList.toggle("dark-theme");
 
     if (document.body.classList.contains("dark-theme")) {
         document.querySelector("nav i").classList.replace("fa-moon", "fa-sun");
         document.querySelector("nav i").style.color = "white";
+        localStorage.setItem("theme", "dark");
     } else {
         document.querySelector("nav i").classList.replace("fa-sun", "fa-moon");
         document.querySelector("nav i").style.color = "black";
+        localStorage.setItem("theme", "light");
     }
-})
+});
